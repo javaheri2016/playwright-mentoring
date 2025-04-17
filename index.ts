db16f715d98e92f2fe41963af058f61b20ff1b34
Katarzyna <katarzynapodgorska@poczta.fm>
2025-04-17 22:08:39 +0200

    Dodanie sciezki do katalogu ze zdjęciami

--- index.ts
+++ index.ts
@@ -4,6 +4,6 @@ import * as pw from 'playwright';
   const browser = await pw.chromium.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('https://javaheri.pl');
-  await page.screenshot({ path: 'screenshot.png' });
+  await page.screenshot({ path: 'photo/screenshot.png' });
   await browser.close();
 })();
