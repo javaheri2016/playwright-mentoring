5bd6d94269ec3bfbf2c594a0fd32713904fdae2e
Katarzyna <katarzynapodgorska@poczta.fm>
2025-07-14 21:58:15 +0200

    Testy poprawności kodu do pobierania pliku MP3

---
+++ tests/portugate.spec.ts
@@ -0,0 +1,46 @@
+import { test, expect } from '@playwright/test';
+
+
+const validCode = 'PORTU2025';
+const lowercaseCode = validCode.toLowerCase();
+
+
+test('wprowadzenie poprawnego kodu na stronie ', async ({ page }) => {
+  await page.goto('https://portugate.pt/audio/')
+  const input = page.locator('#audio-code');
+  await input.fill(validCode);
+  const submitButton = page.locator('#audio-form > p > button')
+  await submitButton.click();
+  const downloadLink=page.locator('#download-link')
+  await expect(downloadLink).toBeVisible ();
+  const textContent = await downloadLink.textContent();
+console.log('Zawartość linku:', textContent);
+});
+
+
+test('wprowadzenie kodu z małych liter na stronie ', async ({ page }) => {
+  await page.goto('https://portugate.pt/audio/')
+  const input = page.locator('#audio-code');
+  await input.fill(lowercaseCode);
+  const submitButton = page.locator('#audio-form > p > button')
+  await submitButton.click();
+  const downloadLink=page.locator('#download-link')
+  await expect(downloadLink).toContainText("❌ Invalid code.")
+
+const textContent = await downloadLink.textContent();
+console.log('Zawartość linku:', textContent);
+});
+
+
+test('wprowadzenie blednego kodu na stronie ', async ({ page }) => {
+  await page.goto('https://portugate.pt/audio/')
+  const input = page.locator('#audio-code');
+  await input.fill('11111111aaaaaa');
+  const submitButton = page.locator('#audio-form > p > button')
+  await submitButton.click();
+  const downloadLink=page.locator('#download-link')
+  await expect(downloadLink).toContainText("❌ Invalid code.")
+
+const textContent = await downloadLink.textContent();
+console.log('Zawartość linku:', textContent);
+});
