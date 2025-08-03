c283087f2ed8a308358d06f03ea4d094351f8cca
Katarzyna <katarzynapodgorska@poczta.fm>
2025-08-03 13:28:10 +0200

    Add  file with pasword  generator password and apply in test file

--- tests/portugate4.spec.ts
+++ tests/portugate4.spec.ts
@@ -1,12 +1,14 @@
 import { test, expect, Page } from '@playwright/test';
+import { generateRandomPassword } from '../utils/passwordGenerator.ts';
 
 const validCode = 'PORTU2025';
-const invalidCode = '11111111aaaaaa'
+const randomPassword = generateRandomPassword(6);
 
 async function submitCode(page: Page, code: string) {
   await page.goto('https://portugate.pt/audio/');
   const input = page.locator('#audio-code');
-  await input.fill(code);
+  await input.fill(randomPassword);
+  await page.screenshot({ path: 'photo/screenshot.png'});
   const submitButton = page.locator('#audio-form > p > button');
   await submitButton.click();
   return page.locator('#download-link');
@@ -26,13 +28,13 @@ test('Submitting a valid code', async ({ page }, testInfo) => {
 });
 
 test('Submitting a code in lowercase', async ({ page }, testInfo) => {
-  const downloadLink = await submitCode(page, validCode.toLowerCase());
+  const downloadLink = await submitCode(page, randomPassword.toLowerCase());
   await assertContainsAndLog(downloadLink,'Invalid code', testInfo);
   
 });
 
 test('Submitting an invalid code', async ({ page }, testInfo) => {
-  const downloadLink = await submitCode(page, invalidCode);
+  const downloadLink = await submitCode(page, randomPassword);
   await assertContainsAndLog(downloadLink,'Invalid code',testInfo);
 });
 
---
+++ utils/passwordGenerator.ts
@@ -0,0 +1,9 @@
+export function generateRandomPassword(length: number = 6): string {
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=';
+  let password = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    password += chars[randomIndex];
+  }
+  return password;
+}
